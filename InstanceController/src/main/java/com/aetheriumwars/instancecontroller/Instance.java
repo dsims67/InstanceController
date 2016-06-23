d278174edbb5c4bcdbed1d5500245826facdcd41
Savior67 <savior0067@gmail.com>
2016-06-23 07:10:48 -0500

    - started instance object

--- InstanceController/src/main/java/com/aetheriumwars/instancecontroller/Instance.java
+++ InstanceController/src/main/java/com/aetheriumwars/instancecontroller/Instance.java
@@ -1,10 +1,20 @@
 package com.aetheriumwars.instancecontroller;
 
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.PropertiesCredentials;
+import com.amazonaws.services.ec2.AmazonEC2Client;
+
 public class Instance {
 //will represent a single mc server instance of a particular type
 //instance types will be defined in a configuration file
+	private AmazonEC2Client ec2Client;
 	
 	public Instance() {
+		//retrieve the credentials
+		AWSCredentials credentials = new PropertiesCredentials(
+		         AwsConsoleApp.class.getResourceAsStream("AwsCredentials.properties"));
 		
+		ec2Client = new AmazonEC2Client(credentials);
+		ec2Client.
 	}
 }
